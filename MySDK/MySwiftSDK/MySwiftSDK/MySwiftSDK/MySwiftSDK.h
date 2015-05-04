c19d721f70b9dbcde2744787a91c31ddb8455b76
Alexander Tsepkov <atsepkov@atsepkov-mbp15.local>
2015-05-03 22:56:04 -0400

    Updated gitignore

---
+++ MySDK/MySwiftSDK/MySwiftSDK/MySwiftSDK/MySwiftSDK.h
@@ -0,0 +1,19 @@
+//
+//  MySwiftSDK.h
+//  MySwiftSDK
+//
+//  Created by Alexander Tsepkov on 5/2/15.
+//  Copyright (c) 2015 Alexander Tsepkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MySwiftSDK.
+FOUNDATION_EXPORT double MySwiftSDKVersionNumber;
+
+//! Project version string for MySwiftSDK.
+FOUNDATION_EXPORT const unsigned char MySwiftSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySwiftSDK/PublicHeader.h>
+
+
---
+++ MySDK/MySwiftSDK/MySwiftSDK/MySwiftSDK/SearchBar.swift
@@ -0,0 +1,9 @@
+//
+//  SearchBar.swift
+//  MySwiftSDK
+//
+//  Created by Alexander Tsepkov on 5/2/15.
+//  Copyright (c) 2015 Alexander Tsepkov. All rights reserved.
+//
+
+import Foundation
